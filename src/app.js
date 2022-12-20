2bea2d4d76fdc5a652ef68b7a82004fcbfbc1a36
Kelthaz <davidkelthaz@gmail.com>
2022-12-19 20:05:51 -0500

    Cors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ import { PORT } from './config.js'
 
 const app = express()
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
+
 app.get('/', async (req, res) => {
     const rows = await BD.query('SELECT * from peliculas')
     res.json(rows)
